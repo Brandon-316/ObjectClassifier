630071d663d30ae4ad91878ea3a8c1223c45be44
Brandon Mahoney <willb31681@gmail.com>
2019-02-27 21:49:48 -0600

    Initial commit

--- ObjectClassifier/ViewController.swift
+++ ObjectClassifier/ViewController.swift
@@ -7,14 +7,74 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let imagePicker = UIImagePickerController()
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var classificationLabel: UILabel!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
     }
+    
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+            guard let ciImage = CIImage(image: userPickedImage) else { fatalError("Could not convert UIImage into CIImage.") }
+            
+            detect(image: ciImage)
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else { fatalError("Loading CoreML Model failed.") }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else { fatalError("Model failed to process image.") }
+            
+            if let firstResult = results.first {
+                let confidence = firstResult.confidence * 100
+                let confidenceString = String(format:"%.2f", confidence)
+                self.classificationLabel.isHidden = false
+                self.classificationLabel.text = "\nYour Result:\n\(firstResult.identifier)\nConfidence: \(confidenceString)%\n"
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+        
+    }
+    
 
+    
+    @IBAction func selectImage(_ sender: UIBarButtonItem) {
+        if sender.tag == 1 {
+            imagePicker.sourceType = .photoLibrary
+        } else {
+            imagePicker.sourceType = .camera
+        }
+        present(imagePicker, animated: true, completion: nil)
+    }
 
 }
 
